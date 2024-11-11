821cd99c71ed9ff4aad3c9e70432209dc0846c3c
konnofuente <95084135+konnofuente@users.noreply.github.com>
2024-11-11 07:25:21 +0100

    inits

---
+++ README.md
@@ -0,0 +1,80 @@
+<h1 align="center">Minstat Dashboard Dashboard Admin</h1>
+
+
+## Installation
+
+<p>1. clone this repo first or download zip code </p>
+
+```
+git clone https://github.com/konnofuente/Minstats-Dashboard.git
+```
+
+<p>2. Open Minstat folder and follow the step </p>
+
+## Step to run this app
+
+## 1. Project setup
+
+<p>before use,  install all dependencies app first with command :</p>
+
+using npm :
+
+```
+npm install
+```
+
+using yarn :
+
+```
+yarn install
+```
+
+### 2. Compiles and hot-reloads for development
+
+<p>run this command to compile code :</p>
+
+using npm :
+
+```
+npm run serve
+```
+
+using yarn :
+
+```
+yarn serve
+```
+
+### 3. Compiles and minifies for production
+
+<p>build your app project to production with command :</p>
+
+using npm :
+
+```
+npm run build
+```
+
+using yarn :
+
+```
+yarn build
+```
+
+## Open Contribute
+
+This repository ready to contribute
+
+##### Contribute Guide
+
+- Fork this repository
+- `npm install` or `yarn install`
+- make change
+- open pull request
+
+## Author
+
+Minstats is created by <a href="https://github.com/konnofuente/">Konno Meli.</a>
+
+## License
+
---
+++ src/components/IconDataCard.vue
@@ -0,0 +1,43 @@
+<!-- IconDataCard.vue -->
+<template>
+    <div :class="`bg-${bgColor} w-full rounded-md p-5 border dark:border-gray-700 grid justify-start `">
+      <div class="p-2 max-w-sm">
+        <div :class="`bg-${iconBgColor} rounded-full w-14 h-14 text-lg p-3 `" :style="{ color: iconColor }">
+          <slot name="icon"></slot>
+        </div>
+      </div>
+      <div class="block p-2 w-full">
+        <p class="font-semibold text-gray-900 dark:text-gray-200 text-xl">{{ title }}</p>
+        <h2 class="font-normal text-gray-400 text-md mt-1">{{ subtitle }}</h2>
+        <p class="text-blue-500 text-sm mt-1">{{ additionalInfo }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "IconDataCard",
+    props: {
+      title: String,
+      subtitle: String,
+      additionalInfo: String,
+      bgColor: {
+        type: String,
+        default: "white",
+      },
+      iconBgColor: {
+        type: String,
+        default: "gray-200",
+      },
+      iconColor: {
+        type: String,
+        default: "black",
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any component-specific styling here if needed */
+  </style>
+  
